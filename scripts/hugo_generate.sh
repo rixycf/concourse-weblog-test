6d042588d3a79251e77ae62d89db955804a6a3c0
Riki Kaida <kasnake1013@gmail.com>
2018-05-15 00:33:22 +0900

    fix scripts

--- scripts/hugo_generate.sh
+++ scripts/hugo_generate.sh
@@ -7,6 +7,8 @@ pwd
 git clone public public_modified
 cd public_modified
 pwd
+git config --global user.email "kasnake1013@gmail.com"
+git config --global user.name "rixycf"
 git add .
 git commit -m "concourse test"
 # mv resource-hugo/public resource-hugo/public_modified
