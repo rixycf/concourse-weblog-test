c5696cdde7cecca867e82d5a0f77ee1a8f17c93e
Riki Kaida <kasnake1013@gmail.com>
2018-05-15 00:25:51 +0900

    fix git clone at script

--- scripts/hugo_generate.sh
+++ scripts/hugo_generate.sh
@@ -2,8 +2,11 @@
 # generate files
 hugo -s resource-hugo
 ls -alt resource-hugo/public
-mv resource-hugo/public resource-hugo/public_modified
-cd resource-hugo
+git clone public public_modified
+cd public_modified
+git add .
+git commit -m "concourse test"
+# mv resource-hugo/public resource-hugo/public_modified
 ls -lat 
 
 # cd resource-hugo/public
