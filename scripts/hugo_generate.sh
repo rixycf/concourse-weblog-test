178a2c08014cee843e20162e367914c1c227e943
Riki Kaida <kasnake1013@gmail.com>
2018-05-16 00:07:11 +0900

    change git clone

--- scripts/hugo_generate.sh
+++ scripts/hugo_generate.sh
@@ -7,17 +7,25 @@ hugo -s resource-hugo
 cd resource-hugo
 pwd
 
-git clone public ../public_modified
-cd ../public_modified
-
+cd public
 echo "test" >> test.txt
-pwd
-
 git config --global user.email "kasnake1013@gmail.com"
 git config --global user.name "rixycf"
 git status
 git add .
 git commit -m "concourse test"
 
-# cd resource-hugo/public
+cd ..
+
+git clone public ../public_modified
+# cd ../public_modified
+#
+# echo "test" >> test.txt
+# pwd
+#
+# git config --global user.email "kasnake1013@gmail.com"
+# git config --global user.name "rixycf"
+# git status
 # git add .
+# git commit -m "concourse test"
+
